4a51b8f1ebd5d706502ca2b3332f9390491d9639
unknown <longtp@gmail.com>
2023-07-23 15:42:38 +0700

    server

--- src/requests/requestMethods.jsx
+++ src/requests/requestMethods.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://trackr-api.onrender.com/api";
-const BASE_URL = "http://localhost:9000/api";
+const BASE_URL = "https://trackr-api.onrender.com/api";
+// const BASE_URL = "http://localhost:9000/api";
 // const BASE_URL = "http://192.168.2.90:9000/api";
 
 
